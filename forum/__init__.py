9076928fb7a6f2f8a96d8aee37d9cf9c1e0dd2f7
Yuang Fan <yuangf2@illinois.edu>
2022-10-07 22:13:43 -0500

    Linking frontend basics and backend

--- forum/__init__.py
+++ forum/__init__.py
@@ -1,10 +1,11 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True,
+                template_folder='./templates', static_folder='./templates/static')
     app.config.from_mapping(
         SECRET_KEY='dev',
         # DATABASE=
@@ -23,5 +24,21 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/notification')
+    def notification():
+        return render_template('notification.html')
+
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
+
+    @app.route('/search')
+    def search():
+        return render_template('search.html')
 
     return app
