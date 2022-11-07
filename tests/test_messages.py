e6d0b822dbfb5c09c3df0e87f3e9a647f7cb40f5
Kaiwen-UIUC <73098288+Kaiwen-UIUC@users.noreply.github.com>
2022-11-07 01:39:41 -0600

    test_add_connection function in test_messages done

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -3,6 +3,83 @@ from forum.models.message import delete_all_pm
 from forum.models.authentication import delete_all_user
 
 
+# Test add connection function
+def test_add_connection(client):
+    response = client.post(
+        '/user/register', data={'username': 'zhangsan', 'password': '123'})
+    assert (response.status_code == 302)
+    response = client.post(
+        '/user/login', data={'username': 'zhangsan', 'password': '123'})
+    assert (response.status_code == 302)
+    response = client.post(
+        '/user/register', data={'username': 'lisi', 'password': '123'})
+    assert (response.status_code == 302)
+    response = client.get('/pm/message')
+    assert (response.status_code == 200)
+    response = client.get('/pm/connection_management')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'username': 'nomatch'})
+    assert (response.status_code == 200)
+    messages = get_flashed_messages()
+    assert (messages[0] == "No matching result")
+    response = client.post(
+        '/pm/connection_management', data={'username': 'lisi'})
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'send_connection': 'lisi'})
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'send_connection': 'lisi'})
+    assert (response.status_code == 200)
+    messages = get_flashed_messages()
+    assert (messages[0] == "Connection request already exist")
+    response = client.get('/user/logout')
+    assert (response.status_code == 302)
+    response = client.post(
+        '/user/login', data={'username': 'lisi', 'password': '123'})
+    assert (response.status_code == 302)
+    response = client.get('/pm/message')
+    assert (response.status_code == 200)
+    response = client.get('/pm/connection_management')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'decline_connection': 'zhangsan'})
+    assert (response.status_code == 200)
+    response = client.get('/user/logout')
+    assert (response.status_code == 302)
+    response = client.post(
+        '/user/login', data={'username': 'zhangsan', 'password': '123'})
+    response = client.get('/pm/message')
+    assert (response.status_code == 200)
+    response = client.get('/pm/connection_management')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'username': 'lisi'})
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'send_connection': 'lisi'})
+    assert (response.status_code == 200)
+    response = client.get('/user/logout')
+    assert (response.status_code == 302)
+    response = client.post(
+        '/user/login', data={'username': 'lisi', 'password': '123'})
+    assert (response.status_code == 302)
+    response = client.get('/pm/message')
+    assert (response.status_code == 200)
+    response = client.get('/pm/connection_management')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'accept_connection': 'zhangsan'})
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'delete_connection': 'zhangsan'})
+    assert (response.status_code == 200)
+
+    delete_all_pm()
+    delete_all_user()
+
+
 # Test private message function
 def test_private_message(client):
     response = client.post(
@@ -14,16 +91,37 @@ def test_private_message(client):
     response = client.post(
         '/user/register', data={'username': 'lisi', 'password': '123'})
     assert (response.status_code == 302)
-    # add_connection('zhangsan', 'lisi')
-    # response = client.get(
-    #     '/pm/message?conversation=lisi')
-    # assert (response.status_code == 200)
-    # response = client.post(
-    #     '/pm/message?conversation=lisi', data={'content': 'This is a private message'})
-    # assert (response.status_code == 200)
-    # response = client.get(
-    #     '/pm/message?conversation=lisi')
-    # assert (response.status_code == 200)
+    response = client.get('/pm/message')
+    assert (response.status_code == 200)
+    response = client.get('/pm/connection_management')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'username': 'lisi'})
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'send_connection': 'lisi'})
+    assert (response.status_code == 200)
+    response = client.get('/user/logout')
+    assert (response.status_code == 302)
+    response = client.post(
+        '/user/login', data={'username': 'lisi', 'password': '123'})
+    assert (response.status_code == 302)
+    response = client.get('/pm/message')
+    assert (response.status_code == 200)
+    response = client.get('/pm/connection_management')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/connection_management', data={'accept_connection': 'zhangsan'})
+    assert (response.status_code == 200)
+    response = client.get(
+        '/pm/message?conversation=zhangsan')
+    assert (response.status_code == 200)
+    response = client.post(
+        '/pm/message?conversation=zhangsan', data={'content': 'This is a private message'})
+    assert (response.status_code == 200)
+    response = client.get(
+        '/pm/message?conversation=zhangsan')
+    assert (response.status_code == 200)
 
     delete_all_pm()
     delete_all_user()
